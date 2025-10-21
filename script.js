2d0b11e519e86b4d4278db1758f519e64b132205
Tewodros T. Bekere <tewodros.bekere16@gmail.com>
2025-10-21 23:43:18 +0100

    Import additional storage functions for user data management

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js";
+import { getUserIds, getData, setData } from "./storage.js";
 
 /**
  * Sort bookmarks in reverse chronological order (newest first) by createdAt ISO string
